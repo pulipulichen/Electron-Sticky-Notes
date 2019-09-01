f64de206a256f68b4c5d37c538f0a28f23772acd
Pulipuli Chen <blog@pulipuli.info>
2019-09-01 23:11:44 +0800

    0901 #4 換成production還是不行，繼續回到development研究

--- app/webpack/src/components/MenuBar/MenuBar.vue
+++ app/webpack/src/components/MenuBar/MenuBar.vue
@@ -1,4 +1,4 @@
 <template src="./MenuBar.html"></template>
-<script src="./MenuBar.js"></script>
 <style src="./MenuBar.less" lang="less" scoped></style>
 <i18n src="./MenuBar.json"></i18n>
+<script src="./MenuBar.js"></script>
