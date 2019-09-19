8691704e509bfe0c9d363275ebc1ab1c7ddd8136
Pulipuli Chen <blog@pulipuli.info>
2019-09-20 04:41:01 +0800

    0920 刪除多餘資料

--- static-app/components/MenuBar/MenuBar.vue
+++
@@ -1,3 +0,0 @@
-<template src="./components/MenuBar/MenuBar.html"></template>
-<script src="./components/MenuBar/MenuBar.js"></script>
-<style src="./components/MenuBar/MenuBar.less" lang="less"></style>
